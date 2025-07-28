f33e8b3021761f88e599282ffbe7930883c01e67
ranjit2602 <ranjitshekar@gmail.com>
2025-07-28 15:05:57 +0530

    smoothening graph

--- powerpedal_test_dashboard.py
+++ powerpedal_test_dashboard.py
@@ -22,11 +22,7 @@ st.set_page_config(
 # Display logo and title
 col1, col2 = st.columns([1, 5])
 with col1:
-<<<<<<< HEAD
     st.image("https://raw.githubusercontent.com/ranjit2602/powerpedal_test_dashboard/main/logo.png", width=400)
-=======
-    st.image("https://raw.githubusercontent.com/ranjit2602/powerpedal_test_dashboard/main/logo.png", width=500)  # Balanced logo size
->>>>>>> 980da9add7597582f1b8101d40bea7271bb7a216
 with col2:
     st.markdown("<h1 style='margin-top: 20px;'>PowerPedal™ Test Results Dashboard</h1>", unsafe_allow_html=True)
 
